497afd44a0ed55a5fe190e9dc1641dfbd5064c12
SirMoistPotatoViper <60393869+SirMoistPotatoViper@users.noreply.github.com>
2024-03-18 12:49:44 -0500

    Framework

---
+++ Assets/Scripts/MenuControls.cs
@@ -0,0 +1,101 @@
+/*****************************************************************************
+// File Name : MenuControls.cs
+// Author : Jake Slutzky
+// Creation Date : March 18, 2024
+//
+// Brief Description : This script controls the main menu and allows the player to press the quit, start, 
+                       and other menu buttons.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    [SerializeField] GameObject _ControlsPanel;
+    [SerializeField] GameObject _CreditsPanel;
+    //[SerializeField] GameObject _closeControlsButton;
+    //[SerializeField] GameObject _closeCreditsButton;
+    [SerializeField] GameObject _startButton;
+    [SerializeField] GameObject _controlsButton;
+    [SerializeField] GameObject _creditsButton;
+    [SerializeField] GameObject _quitButton;
+
+    /// <summary>
+    /// When this void is triggered, the game will load the next scene
+    /// Eventually it will also trigger a menu transition of some sort prior to the next scene loading
+    /// </summary>
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// When this void is triggered, the game will close itself
+    /// </summary>
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// when this void is triggered, the game will load the "Menu" scene
+    /// </summary>
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    /// <summary>
+    /// When this void is trigged, it will Open the controls panel
+    /// </summary>
+    public void OpenControls()
+    {
+        _ControlsPanel.SetActive(true);
+        //_closeControlsButton.SetActive(true);
+        _startButton.SetActive(false);
+        _controlsButton.SetActive(false);
+        _creditsButton.SetActive(false);
+        _quitButton.SetActive(false);
+    }
+
+    /// <summary>
+    /// When this void is trigged, it will Close the controls panel
+    /// </summary>
+    public void CloseControls()
+    {
+        _ControlsPanel.SetActive(false);
+        //_closeControlsButton.SetActive(false);
+        _startButton.SetActive(true);
+        _controlsButton.SetActive(true);
+        _creditsButton.SetActive(true);
+        _quitButton.SetActive(true);
+    }
+
+    /// <summary>
+    /// When this void is trigged, it will Open the credits panel
+    /// </summary>
+    public void OpenCredits()
+    {
+        _CreditsPanel.SetActive(true);
+        //_closeCreditsButton.SetActive(true);
+        _startButton.SetActive(false);
+        _controlsButton.SetActive(false);
+        _creditsButton.SetActive(false);
+        _quitButton.SetActive(false);
+    }
+
+    /// <summary>
+    /// When this void is trigged, it will Close the credit panel
+    /// </summary>
+    public void CloseCredits()
+    {
+        _CreditsPanel.SetActive(false);
+        //_closeCreditsButton.SetActive(false);
+        _startButton.SetActive(true);
+        _controlsButton.SetActive(true);
+        _creditsButton.SetActive(true);
+        _quitButton.SetActive(true);
+    }
+}
